d569a8556b607b77f32c4911a492e6e8e1b68c60
Jared Galanis <jgalani2@jhu.edu>
2022-09-19 13:47:20 -0400

    WIP

---
+++ sp/app.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const path = require("path");
+const passport = require("passport");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const httpProxy = require('http-proxy');
+const ensureAuthenticated = require('./middleware/ensure-auth');
+
+var env = process.env.NODE_ENV || "development";
+const config = require("./config/config")[env];
+
+console.log("Using configuration :", config);
+
+require("./config/passport")(passport, config);
+
+var app = express();
+
+// create a proxy server for each of the pass services
+const apiProxy = httpProxy.createProxyServer();
+const fcrepo = 'http://fcrepo:8080/fcrepo',
+      userService = 'http://fcrepo:8080/',
+      ember = 'http://ember:81/',
+      elasticSearch = 'http://elasticsearch:9200/',
+      schemaService = 'http://schemaservice:8086',
+      policyService = 'http://policyservice:8088',
+      doiService = 'http://doiservice:8080/',
+      downloadService = 'http://downloadservice:6502';
+
+app.set("port", config.app.port);
+app.set("views", __dirname + "/app/views");
+app.set("view engine", "pug");
+app.use(morgan("combined"));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET || "This is not a secret, friend."
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+require("./config/routes")(app, config, passport);
+
+app.all("/fcrepo/*", ensureAuthenticated, function(req, res) {
+  apiProxy.web(req, res, {target: fcrepo});
+});
+
+app.all("/pass-user-service/*", ensureAuthenticated, function(req, res) {
+  const user = req.user;
+
+  req.headers['DISPLAY_NAME_HEADER'] = user.displayName;
+  req.headers['EMAIL_HEADER'] = user.email;
+  req.headers['EPPN_HEADER'] = user.eppn;
+  req.headers['GIVENNAME_HEADER'] = user.givenName;
+  req.headers['SN_HEADER'] = user.surname;
+  req.headers['SCOPED_AFFILIATION_HEADER'] = user.scopedAffiliation;
+  req.headers['EMPLOYEE_ID_HEADER'] = user.employeeNumber;
+  req.headers['HOPKINS_ID_HEADER'] = user.uniqueId;
+  req.headers['EMPLOYEE_ID_TYPE'] = user.employeeIdType;
+  req.headers['HOPKINS_ID_TYPE'] = user.uniqueIdType;
+  req.headers['JHED_ID_TYPE'] = user.jhedIdType;
+
+  apiProxy.web(req, res, {target: userService});
+});
+
+app.all("/app/*", ensureAuthenticated, function(req, res) {
+  apiProxy.web(req, res, {target: ember});
+});
+
+app.all("/pass/_search/*", ensureAuthenticated, function(req, res) {
+  apiProxy.web(req, res, {target: elasticSearch});
+});
+
+app.all("/schemaservice/*", ensureAuthenticated, function(req, res) {
+  apiProxy.web(req, res, {target: schemaService});
+});
+
+app.all("/policyservice/*", ensureAuthenticated, function(req, res) {
+  apiProxy.web(req, res, {target: policyService});
+});
+
+app.all("/doiservice/*", ensureAuthenticated, function(req, res) {
+  apiProxy.web(req, res, {target: doiService});
+});
+
+app.all("/downloadservice/*", ensureAuthenticated, function(req, res) {
+  apiProxy.web(req, res, {target: downloadService});
+});
+
+app.listen(app.get("port"), function() {
+  console.log("Express server listening on port " + app.get("port"));
+});
---
+++ sp/config/config.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+module.exports = {
+  development: {
+    app: {
+      name: "Passport SAML strategy",
+      port: process.env.PORT || 80
+    },
+    passport: {
+      strategy: process.env.PASSPORT_STRATEGY || "saml",
+      saml: {
+        path: "/auth/Shibboleth.sso/SAML2/POST",
+        identifierFormat: process.env.IDENTIFIER_FORMAT || "",
+        entryPoint:
+          process.env.SAML_ENTRY_POINT ||
+          "https://pass.local/idp/profile/SAML2/Redirect/SSO",
+        issuer: process.env.SAML_ISSUER || "https://sp.pass/shibboleth",
+        decryptionPvk: fs.readFileSync("privateKey.key", "utf-8"),
+        cert: process.env.SAML_CERT || "MIIDFDCCAfygAwIBAgIVAN3vv+b7KN5Se9m1RZsCllp/B/hdMA0GCSqGSIb3DQEBCwUAMBUxEzARBgNVBAMMCmlkcHRlc3RiZWQwHhcNMTUxMjExMDIyMDE0WhcNMzUxMjExMDIyMDE0WjAVMRMwEQYDVQQDDAppZHB0ZXN0YmVkMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAh91caeY0Q85uhaUyqFwP2bMjwMFxMzRlAoqBHd7gu6eo4duaeLz1BaoR2XTBpNNvFR5oHH+TkKahVDGeH5+kcnIpxI8JPdsZml1srvf2Z6dzJsulJZUdpqnngycTkGtZgEoC1vmYVky2BSAIIifmdh6s0epbHnMGLsHzMKfJCb/Q6dYzRWTCPtzE2VMuQqqWgeyMr7u14x/Vqr9RPEFsgY8GIu5jzB6AyUIwrLg+MNkv6aIdcHwxYTGL7ijfy6rSWrgBflQoYRYNEnseK0ZHgJahz4ovCag6wZAoPpBsuYlY7lEr89Ucb6NHx3uqGMsXlDFdE4QwfDLLhCYHPvJ0uwIDAQABo1swWTAdBgNVHQ4EFgQUAkOgED3iYdmvQEOMm6u/JmD/UTQwOAYDVR0RBDEwL4IKaWRwdGVzdGJlZIYhaHR0cHM6Ly9pZHB0ZXN0YmVkL2lkcC9zaGliYm9sZXRoMA0GCSqGSIb3DQEBCwUAA4IBAQBIdd4YWlnvJjql8+zKKgmWgIY7U8DA8e6QcbAf8f8cdE33RSnjI63Xsv/y9GfmbAVAD6RIAXPFFeRYJ08GOxGI9axfNaKdlsklJ9bk4ducHqgCSWYVer3sRQBjxyOfSTvk9YCJvdJVQRJLcCvxwKakFCsOSnV3t9OvN86Ak+fKPVB5j2fM/0fZKqjn3iqgdNPTLXPsuJLJO5lITRiBa4onmVelAiCstI9PQiaEck+oAHnMTnC9JE/BDHv3e4rwq3LznlqPw0GSd7xqNTdMDwNOWjkuOr3sGpWS8ms/ZHHXV1Vd22uPe70is00xrv14zLifcc8oj5DYzOhYRifRXgHX",
+      },
+    }
+  }
+};
